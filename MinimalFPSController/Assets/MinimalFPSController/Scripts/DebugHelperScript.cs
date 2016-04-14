d6fc36fa4c9e821e0fd84c4ab1149659f277a6e7
Richard Walsh <git@whilefun.com>
2016-04-14 16:01:33 -0400

    Version 1.0
    Includes basic test scene

---
+++ MinimalFPSController/Assets/MinimalFPSController/Scripts/DebugHelperScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugHelperScript : MonoBehaviour {
+
+	public GameObject thePlayer;
+
+	void Start(){
+	
+	}
+	
+	void Update(){
+
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Application.Quit();
+		}
+
+		if(Input.GetKeyDown(KeyCode.M)){
+			thePlayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.MouseLook>().enableMouseLook = !thePlayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.MouseLook>().enableMouseLook;
+		}
+
+		if(Input.GetKeyDown(KeyCode.V)){
+			thePlayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enableMovement = !thePlayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enableMovement;
+		}
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			thePlayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enableRunning = !thePlayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enableRunning;
+		}
+
+
+	}
+
+	void OnGUI(){
+
+		GUI.contentColor = Color.yellow;
+		GUI.Box(new Rect(0,0,256,64),"");
+		GUI.Label(new Rect(0,0,256,64),"M: Toggle MouseLook\nV: Toggle Player Movement\nR: Toggle Player Running Allowed");
+
+	}
+
+}
---
+++ MinimalFPSController/README.txt
@@ -0,0 +1,18 @@
+//
+// Minimal FPS Controller
+// A minimal enhanced set of FPS Player Controller functionality in a small test project.
+// http://whilefun.com
+//
+
+This project is compatible with Unity 4.6.3 and Unity 5. It is a reduced and slightly enhanced version of the Unity 5 Standard Asset FPS Character Controller.
+
+Key Additions:
+-Toggles for MouseLook, Player Movement, and Running
+-Slightly quieter footstep and jump/land sounds
+-Reduced "View Bob"
+
+
+Enjoy!
+Richard
+
+@whilefun
