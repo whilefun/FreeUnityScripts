a6b61ac87dfa76c8990041d097754ee432d38239
Richard Walsh <git@whilefun.com>
2018-06-08 17:08:21 -0400

    -Baseline commit for Material Creator. Tested and working as of Unity 2018.1.0f2. Might work in older versions, but has not been tested with older versions.

---
+++ MaterialCreator/MaterialCreator/Scripts/Editor/MaterialsCreator.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+//
+// MaterialsCreator
+// A very simple Editor script to create Materials from Textures.
+//
+// Made with Unity 2018.1.0f2. Probably works in older versions, but was not tested with them.
+//
+// Usage:
+// Select one or more Textures (e.g. in Assets/MyProjectFolder/Textures/)
+// Select Tools -> Create Materials From Selected Textures
+// The Textures will be created in a SIBLING folder called [MATERIAL_FOLDER_NAME] (Default: "Materials")
+//
+public class MaterialsCreator : Editor
+{
+
+    private static readonly string MATERIAL_FOLDER_NAME = "Materials";
+
+    [MenuItem("Tools/Create Materials From Selected Textures")]
+    static void CreateMaterials()
+    {
+
+        try
+        {
+
+            AssetDatabase.StartAssetEditing();
+
+            // Get all Textures from current editor selection
+            Texture[] textures = Selection.GetFiltered<Texture>(SelectionMode.Assets);
+
+            foreach (Texture tex in textures)
+            {
+
+                // Yes, this IS a lot of string declarations. But it works, so I cannot be arsed to make it more 
+                // compact. Just don't put this code in a self-driving car or rocket ship or anything.
+                string rawTexturePath = AssetDatabase.GetAssetPath(tex);
+                string textureFilename = rawTexturePath.Substring(rawTexturePath.LastIndexOf("/") + 1);
+                string rawName = textureFilename.Substring(0, textureFilename.LastIndexOf("."));
+                string currentFolderPath = rawTexturePath.Substring(0, rawTexturePath.LastIndexOf("/"));
+                string parentFolderPath = rawTexturePath.Substring(0, currentFolderPath.LastIndexOf("/"));
+                string materialFolderPath = parentFolderPath + "/" + MATERIAL_FOLDER_NAME;
+                string finalNewMaterialFilePath = materialFolderPath + "/" + rawName + ".mat";
+
+                // Create the materials folder as a sibling folder if it's not there already
+                if (AssetDatabase.IsValidFolder(materialFolderPath) == false)
+                {
+                    AssetDatabase.CreateFolder(parentFolderPath, MATERIAL_FOLDER_NAME);
+                }
+
+                if (AssetDatabase.LoadAssetAtPath(finalNewMaterialFilePath, typeof(Material)) != null)
+                {
+                    Debug.LogWarning("MaterialsCreator:: Material already exists for '"+ finalNewMaterialFilePath + "'. Skipping this Texture.");
+                }
+                else
+                {
+
+                    // Create new material based off of the Standard shader, but cheaper (no reflections or other goo)
+                    Material myMaterial = new Material(Shader.Find("Standard"));
+                    myMaterial.SetTexture("_MainTex", tex);
+                    myMaterial.SetFloat("_Glossiness", 0.0f);
+                    myMaterial.SetFloat("_SpecularHighlights", 0.0f);
+                    myMaterial.SetFloat("_GlossyReflections", 0.0f);
+
+                    AssetDatabase.CreateAsset(myMaterial, finalNewMaterialFilePath);
+
+                }
+
+            }
+
+        }
+        catch(Exception e)
+        {
+            Debug.LogError("MaterialsCreator:: Excepton occurred. Reason: " + e.Message);
+        }
+        finally
+        {
+
+            AssetDatabase.StopAssetEditing();
+            AssetDatabase.SaveAssets();
+
+        }
+
+    }
+
+}
---
+++ MaterialCreator/README.txt
@@ -0,0 +1,32 @@
++------------------+
+| Material Creator |
++------------------+
+
+
+Description:
+-----------
+A simple script that creates Materials from selected Textures.
+
+
+How to Use:
+----------
+
+1) Put the MaterialCreator folder into your Assets folder.
+2) Select one or more Textures (e.g. in Assets/MyProjectFolder/Textures/)
+3) Select Tools -> Create Materials From Selected Textures
+
+The Textures will be created in a SIBLING folder called [MATERIAL_FOLDER_NAME] (Default: "Materials")
+
+
+License Terms:
+-------------
+Released under the MIT license. Do whatever you want with it.
+
+
+Scary Legal Jargon:
+------------------
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+Enjoy!
+Richard
